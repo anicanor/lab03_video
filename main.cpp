36a64acb210a1ce724cb48c9b56d0226d689301e
anicanor <anicanor@mail.csuchico.edu>
2018-02-10 20:18:00 -0800

    doner

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main(){
     
     Video video1("Title One", "www.youtube.com/one", "Comment ONE", 1.1, 1);
-    Video video2("TItle Two", "www.youtube.com/two", "Comment TWO", 2.2, 2);
+    Video video2("Title Two", "www.youtube.com/two", "Comment TWO", 2.2, 2);
 
     video1.print();
     video2.print();
